c3710031505c41a3af279b5ee5d75049cbecd336
mbkim95 <mbkim95@naver.com>
2020-02-24 16:48:28 +0900

    Implement ListActivity
    Apply Viewmodel

--- app/src/main/java/com/project/dailymemo/ListActivity.kt
+++ app/src/main/java/com/project/dailymemo/ListActivity.kt
@@ -1,21 +1,44 @@
 package com.project.dailymemo
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import com.project.dailymemo.data.ListViewModel
+import com.project.dailymemo.data.MemoData
 import kotlinx.android.synthetic.main.activity_list.*
+import java.util.*
 
 class ListActivity : AppCompatActivity() {
+    private var viewModel: ListViewModel? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list)
         setSupportActionBar(toolbar)
 
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.contentLayout, MemoListFragment())
+        fragmentTransaction.commit()
+
+        viewModel = application!!.let {
+            ViewModelProvider(viewModelStore, ViewModelProvider.AndroidViewModelFactory(it)).get(
+                ListViewModel::class.java
+            )
+        }
+
         fab.setOnClickListener { view ->
+//            val intent = Intent(applicationContext, DetailActivity::class.java)
+//            startActivity(intent)
+
             // This code is for testing.
-            val intent = Intent(applicationContext, DetailActivity::class.java)
-            startActivity(intent)
+            viewModel!!.let {
+                var memoData = MemoData()
+                memoData.title = "제목 테스트"
+                memoData.summary = "요약내용 테스트"
+                memoData.createdAt = Date()
+
+                it.addMemo(memoData)
+            }
         }
     }
 
