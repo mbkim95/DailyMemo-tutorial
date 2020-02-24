b82918176f676548a876bb1bff6c8a945ce9b79b
mbkim95 <mbkim95@naver.com>
2020-02-24 14:46:07 +0900

    Implement temporary code in ListActivity for testing

--- app/src/main/java/com/project/dailymemo/ListActivity.kt
+++ app/src/main/java/com/project/dailymemo/ListActivity.kt
@@ -1,9 +1,8 @@
 package com.project.dailymemo
 
+import android.content.Intent
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-
 import kotlinx.android.synthetic.main.activity_list.*
 
 class ListActivity : AppCompatActivity() {
@@ -14,8 +13,9 @@ class ListActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
         fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+            // This code is for testing.
+            val intent = Intent(applicationContext, DetailActivity::class.java)
+            startActivity(intent)
         }
     }
 
